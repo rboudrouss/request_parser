8f9e9c52f0fc175b19ddda21e1715a6156cdb1ae
rboudrouss <r.boudrouss@gmail.com>
2022-12-02 00:01:29 +0100

    giving credit

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,3 +1,8 @@
+/*
+This parser combinator made from scratch is greatly inspired from my friends projet in which i helped a bit
+https://gitlab.com/yeetsquared/arcsquared
+https://gitlab.com/yeetsquared/arcsquared/-/project_members
+*/
 export * from "./pComb";
 export * from "./pGen";
 export * from "./pBin"
