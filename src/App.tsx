00a83812af395fd232944d458d5a6f9d9f1113f3
rboudrouss <r.boudrouss@gmail.com>
2022-12-16 15:03:12 +0100

    added random log msgs

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,26 @@ function App() {
   let [data, setData] = useState("");
   let [read, setRead] = useState(false);
   let [result, setResult] = useState<header_type[] | null>(null);
-  console.log("uwu");
+  console.log("app is weady uwu");
 
   let showFile = async (e: any) => {
-    console.log("oowow");
+    console.log("Dwetected a chwange OWO");
     e.preventDefault();
     const reader = new FileReader();
     reader.onload = async (e) => {
       setData(e.target?.result as string);
-      console.log("OWO");
       let parsed = header_parsers.run(
         convertToBin(cleanInput(e.target?.result as string))
       );
       console.log(parsed);
-      if (parsed.isError) alert(parsed.error);
-      else {
+      if (parsed.isError) {
+        console.log("owo niowo, thewe is an ewor 0///0");
+        console.log(parsed.error);
+        alert(parsed.error);
+      } else {
         setRead(true);
         setResult(parsed.result);
+        console.log("File wread awnd pawsed successfuwly UwU");
       }
     };
     reader.readAsText(e.target.files[0]);
@@ -36,7 +39,9 @@ function App() {
   return (
     <div>
       <input type="file" onChange={showFile} />
-        {read && result ? result.map((e) => <p>{to_arrow(e[0], e[3])}</p>) : "No Data"}
+      {read && result
+        ? result.map((e) => <p>{to_arrow(e[0], e[3])}</p>)
+        : "No Data"}
     </div>
   );
 }
