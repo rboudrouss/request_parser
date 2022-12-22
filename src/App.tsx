bd76a445163f735392b7a425d9d598e06954ed1d
rboudrouss <r.boudrouss@gmail.com>
2022-12-22 17:00:38 +0100

    working on the input thingy

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
+import { render } from "react-dom";
 import {
   cleanInput,
   convertToBin,
+  filter,
   header_parsers,
   header_type,
   to_arrow,
 } from "./headerP";
 
+function Arrows(props: { data: header_type[] | null }) {
+  return props.data === null ? (
+    <p>No Data</p>
+  ) : (
+    <>
+      {props.data.map((e) => (
+        <p>{to_arrow(e[0], e[3])}</p>
+      ))}
+    </>
+  );
+}
+
 function App() {
   // let [data, setData] = useState("");
   let [read, setRead] = useState(false);
   let [result, setResult] = useState<header_type[] | null>(null);
+  let [filtered, setFiltered] = useState<header_type[] | null>(null);
   console.log("app is weady uwu");
 
   let showFile = async (e: any) => {
-    console.log("Dwetected a chwange OWO");
     e.preventDefault();
+    console.log("Dwetected a chwange OWO");
     const reader = new FileReader();
     reader.onload = async (e) => {
       // setData(e.target?.result as string);
@@ -30,18 +45,41 @@ function App() {
       } else {
         setRead(true);
         setResult(parsed.result);
+        setFiltered(parsed.result);
         console.log("File wread awnd pawsed successfuwly UwU");
       }
     };
     reader.readAsText(e.target.files[0]);
   };
 
+  let filterInp = async (e: any) => {
+    e.preventDefault();
+
+    console.log(
+      "target",
+      e.target.value,
+      (e.target.value as string).split(" ").map((a) => a.split("="))
+    );
+    if (result) {
+      setFiltered(
+        filter(
+          (e.target.value as string).split("\n").map((a) => a.split("=")),
+          result
+        )
+      );
+    } else console.log("woops, no data cannot filter");
+  };
+
   return (
     <div>
-      <input type="file" onChange={showFile} />
-      {read && result
-        ? result.map((e) => <p>{to_arrow(e[0], e[3])}</p>)
-        : "No Data"}
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="file" onChange={showFile} />
+        <input
+          type="text"
+          onKeyDown={(e) => e.key === "Enter" && filterInp(e)}
+        />
+      </form>
+      <Arrows data={filtered} />
     </div>
   );
 }
